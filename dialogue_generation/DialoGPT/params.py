e5c0f438de28dc6d250039b2c4054707248e0a50
friedrichor <kongfanheng426@163.com>
2023-03-11 19:46:38 +0800

    Update params.py

--- dialogue_generation/DialoGPT/params.py
+++ dialogue_generation/DialoGPT/params.py
@@ -1,22 +1,18 @@
 import os
 import sys
-import argparse
 import torch
 
+epochs = 10
+batch_size = 8
+lr = 5e-4
+weight_decay = 0
 
-parser = argparse.ArgumentParser()
+model_name = 'microsoft/DialoGPT-small'
+tokenizer_name = 'microsoft/DialoGPT-small'
 
-parser.add_argument('--epochs', type=int, default=10)
-parser.add_argument('--batch_size', type=int, default=8)
-parser.add_argument('--lr', type=float, default=5e-4)
-parser.add_argument('--weight_decay', type=float, default=0)
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+num_workers = min([os.cpu_count(), batch_size if batch_size > 1 else 0, 8])
 
-TEXT_DIALOGUE_MODEL = ['microsoft/DialoGPT-small', 'microsoft/DialoGPT-medium', 'microsoft/DialoGPT-large']
-parser.add_argument('--model_name', type=str, choices=TEXT_DIALOGUE_MODEL, default='microsoft/DialoGPT-small')
-parser.add_argument('--tokenizer_name', type=str, default='microsoft/DialoGPT-small')
+data_dir = os.path.join(sys.path[0], 'dataset_partial')  # 数据集目录
+weights_dir = os.path.join(sys.path[0], 'weights')  # 模型权重保存目录
 
-args = parser.parse_args()
-args.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-args.nw = min([os.cpu_count(), args.batch_size if args.batch_size > 1 else 0, 8])  # num_workers
-args.data_dir = os.path.join(sys.path[0], 'dataset_partial')  # 数据集目录
-args.weights_dir = os.path.join(sys.path[0], 'weights')  # 模型权重保存目录
