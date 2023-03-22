ea1c705635b7fb85bca25157b80cde290a64314e
friedrichor <kongfanheng426@163.com>
2023-03-22 21:30:06 +0800

    Create dataset.py

---
+++ text_classification/BERT/dataset.py
@@ -0,0 +1,41 @@
+import torch
+from torch.utils.data import Dataset
+from torch.nn.utils.rnn import pad_sequence
+
+
+class MyDataset(Dataset):
+    def __init__(self, data, tokenizer):
+        self.data = data
+        self.tokenizer = tokenizer
+
+    def __getitem__(self, index):
+        text = self.data[index]["text"]
+        label = self.data[index]["label"]
+
+        text_encoded = self.tokenizer.encode_plus(text=text,
+                                                  add_special_tokens=True,  # 开头加 [CLS], 末尾加 [SEP]
+                                                  return_attention_mask=True)
+
+        return {
+            "input_ids": text_encoded["input_ids"],
+            "attention_mask": text_encoded["attention_mask"],
+            "label": label
+        }
+
+    def __len__(self):
+        return len(self.data)
+
+    def collate_fn(self, batch):
+        input_ids_list = [torch.tensor(instance['input_ids']) for instance in batch]
+        input_ids_pad = pad_sequence(input_ids_list, batch_first=True, padding_value=self.tokenizer.pad_token_id)
+
+        attention_mask_list = [torch.tensor(instance['attention_mask']) for instance in batch]
+        attention_mask_pad = pad_sequence(attention_mask_list, batch_first=True, padding_value=0)
+
+        label_list = [torch.tensor(instance['label']) for instance in batch]
+
+        return {
+            "input_ids": torch.tensor(input_ids_pad),
+            "attention_mask": torch.tensor(attention_mask_pad),
+            "label": torch.tensor(label_list)
+        }
