4fb454749081d9c56e27fa42b0c6c2f277cc214b
friedrichor <kongfanheng426@163.com>
2023-03-23 16:39:14 +0800

    Update model.py

--- text_classification/BERT/model.py
+++ text_classification/BERT/model.py
@@ -5,9 +5,14 @@ from transformers.modeling_outputs import SequenceClassifierOutput
 
 
 class MyBertModel(nn.Module):
-    def __init__(self, pretrained_model_name_or_path, num_classes):
+    def __init__(self, pretrained_model_name_or_path, num_classes, freeze_layers=False):
         super().__init__()
         self.backbone = BertModel.from_pretrained(pretrained_model_name_or_path)
+
+        if freeze_layers:
+            for name, para in self.backbone.named_parameters():
+                if 'pooler' not in name:  # 除了最后一个 pooler 层外，其它层全部冻结
+                    para.requires_grad_(False)
 
         self.fc1 = nn.Linear(self.backbone.pooler.dense.out_features, 128)  # [768, 128]
         self.fc2 = nn.Linear(128, num_classes)  # [768, num_classes]
