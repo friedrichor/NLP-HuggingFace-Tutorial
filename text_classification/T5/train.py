e060e4f0dbe2f9446b2dd1288f04a367e81317fb
friedrichor <kongfanheng426@163.com>
2023-04-03 19:07:53 +0800

    Update train.py

--- text_classification/T5/train.py
+++ text_classification/T5/train.py
@@ -6,9 +6,9 @@ from datetime import datetime
 import argparse
 
 import torch
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, WeightedRandomSampler
 from torch.utils.tensorboard import SummaryWriter
-from transformers.optimization import AdamW, get_linear_schedule_with_warmup
+from transformers.optimization import Adafactor, AdafactorSchedule, AdamW, get_linear_schedule_with_warmup
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 
 import params
@@ -49,18 +49,33 @@ def main(args):
     valid_data = read_json(valid_data_file)
 
     # tokenizer
-    tokenizer = T5Tokenizer.from_pretrained(args.pretrained_model_name_or_path)  # AutoTokenizer
+    tokenizer = T5Tokenizer.from_pretrained(args.pretrained_model_name_or_path)
 
     # classes_map
     classes_map = read_json(args.classes_map_dir)
+    # labels_id_list
+    label_id_list = []
+    for cls in classes_map.keys():
+        labels_id = tokenizer.encode(text=cls, add_special_tokens=False)
+        label_id_list.append(labels_id[0])
 
     # dataset, dataloader
     train_set = MyDataset(train_data, tokenizer, classes_map, args.prefix_text)
+
+    if args.use_weighted_random_sampler:
+        data_label_list = []
+        for data in train_set:
+            data_label_list.append(data['labels'][0])
+        weights = [1.0 / data_label_list.count(label) for label in data_label_list]
+        sampler = WeightedRandomSampler(weights, len(train_data), replacement=False)
+        print("Completed sampling.")
+
     train_loader = DataLoader(train_set,
                               batch_size=args.batch_size,
-                              shuffle=True,
+                              shuffle=False if args.use_weighted_random_sampler else True,
+                              sampler=sampler if args.use_weighted_random_sampler else None,
                               pin_memory=True,
-                              num_workers=args.nw,
+                              num_workers=args.num_workers,
                               collate_fn=train_set.collate_fn,
                               drop_last=True)
 
@@ -77,14 +92,23 @@ def main(args):
     model = T5ForConditionalGeneration.from_pretrained(args.pretrained_model_name_or_path)
     model.to(args.device)
 
-    pg = [p for p in model.parameters() if p.requires_grad]
-    optimizer = AdamW(pg, lr=args.lr, weight_decay=args.weight_decay)
-    lr_scheduler = get_linear_schedule_with_warmup(optimizer=optimizer,
-                                                   num_warmup_steps=len(train_loader),
-                                                   num_training_steps=len(train_loader) * args.epochs)
+    if args.use_Adafactor and args.use_AdafactorSchedule:
+        # https://huggingface.co/docs/transformers/v4.27.2/en/main_classes/optimizer_schedules#transformers.Adafactor
+        optimizer = Adafactor(model.parameters(), scale_parameter=True, relative_step=True, warmup_init=True, lr=None)
+        lr_scheduler = AdafactorSchedule(optimizer)
+    elif args.use_Adafactor and not args.use_AdafactorSchedule:
+        optimizer = Adafactor(model.parameters(), scale_parameter=False, relative_step=False, warmup_init=False, lr=args.learning_rate)
+        lr_scheduler = get_linear_schedule_with_warmup(optimizer=optimizer,
+                                                       num_warmup_steps=args.lr_warmup_steps,
+                                                       num_training_steps=len(train_loader) * args.num_train_epochs)
+    else:
+        optimizer = AdamW(model.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay)
+        lr_scheduler = get_linear_schedule_with_warmup(optimizer=optimizer,
+                                                       num_warmup_steps=args.lr_warmup_steps,
+                                                       num_training_steps=len(train_loader) * args.num_train_epochs)
 
-    best_accuracy = 0
-    for epoch in range(args.epochs):
+    best_macro_f1 = 0
+    for epoch in range(args.num_train_epochs):
         train_result = train_one_epoch(model=model,
                                        device=args.device,
                                        data_loader=train_loader,
@@ -95,13 +119,17 @@ def main(args):
         dev_result = validate(model=model,
                               device=args.device,
                               data_loader=valid_loader,
+                              label_id_list=label_id_list,
                               epoch=epoch)
 
         results = {
             'learning_rate': optimizer.param_groups[0]["lr"],
             'train_loss': train_result['loss'],
             'train_accuracy': train_result['accuracy'],
-            'dev_accuracy': dev_result['accuracy']
+            'dev_accuracy': dev_result['accuracy'],
+            'dev_macro_f1': dev_result['macro_f1'],
+            'dev_micro_f1': dev_result['micro_f1'],
+            'dev_weighted_f1': dev_result['weighted_f1']
         }
 
         logger.info("=" * 100)
@@ -111,11 +139,11 @@ def main(args):
             tb_writer.add_scalar(key, value, epoch)
             logger.info(f"{key}: {value}")
 
-        # 保存在验证集上 accuracy 最高的模型
-        if dev_result['accuracy'] > best_accuracy:
+        # 保存在验证集上 macro_f1 最高的模型
+        if dev_result['macro_f1'] > best_macro_f1:
             torch.save(model.state_dict(), os.path.join(args.save_weights_path, '{}-{}-epoch{}.pth'.format(
                        args.pretrained_model_name_or_path.split('/')[-1], current_time, epoch)))
-            best_accuracy = dev_result['accuracy']
+            best_macro_f1 = dev_result['macro_f1']
 
 
 if __name__ == "__main__":
@@ -126,15 +154,34 @@ if __name__ == "__main__":
     parser.add_argument('--prefix_text', type=str, default=params.prefix_text)
     parser.add_argument('--pretrained_model_name_or_path', type=str, default=params.pretrained_model_name_or_path)
 
-    parser.add_argument('--epochs', type=int, default=params.epochs)
+    parser.add_argument('--num_train_epochs', type=int, default=params.num_train_epochs)
     parser.add_argument('--batch_size', type=int, default=params.batch_size)
-    parser.add_argument('--lr', type=float, default=params.lr)
-    parser.add_argument('--weight_decay', type=float, default=params.weight_decay)
+    parser.add_argument(
+        "--use_weighted_random_sampler", default=False, help="Whether or not to use WeightedRandomSampler."
+    )
 
     parser.add_argument('--device', default=params.device)
-    parser.add_argument('--nw', type=int, default=params.num_workers)
+    parser.add_argument('--num_workers', type=int, default=params.num_workers)
     parser.add_argument('--data_dir', type=str, default=params.data_dir)
     parser.add_argument('--save_weights_path', type=str, default=params.save_weights_path)
+
+    parser.add_argument(
+        "--use_Adafactor", default=True, help="Whether or not to use Adafactor."
+    )
+    parser.add_argument(
+        "--use_AdafactorSchedule", default=True, help="Whether or not to use AdafactorSchedule."
+    )
+    parser.add_argument(
+        '--learning_rate', type=float, default=params.learning_rate,
+        help="Initial learning rate (after the potential warmup period) to use."
+    )
+    parser.add_argument(
+        "--lr_warmup_steps", type=int, default=params.lr_warmup_steps,
+        help="Number of steps for the warmup in the lr scheduler."
+    )
+    parser.add_argument(
+        '--weight_decay', type=float, default=params.weight_decay, help="Weight decay to use."
+    )
 
     args = parser.parse_args()
 
