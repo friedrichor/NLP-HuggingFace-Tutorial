f672504bd0490217dcf1ba85c2e8d783b7b930bc
friedrichor <kongfanheng426@163.com>
2023-04-03 19:06:50 +0800

    Update dataset.py

--- text_classification/T5/dataset.py
+++ text_classification/T5/dataset.py
@@ -11,8 +11,7 @@ class MyDataset(Dataset):
         self.data = data
         self.tokenizer = tokenizer
 
-        self.labels_map = {value: key for key, value in classes_map.items()}
-        # self.labels_map = {0: "anger", 1: "joy", 2: "optimism", 3: "sadness"}
+        self.labels_map = {value: key for key, value in classes_map.items()}  # labels_map = {0: "anger", 1: "joy", 2: "optimism", 3: "sadness"}
         self.prefix_text = prefix_text
 
     def __getitem__(self, index):
@@ -22,7 +21,8 @@ class MyDataset(Dataset):
         text_encoded = self.tokenizer.encode_plus(text=self.prefix_text + text,
                                                   add_special_tokens=True,  # T5没有[CLS]和[SEP]，有结束符</s>
                                                   return_attention_mask=True)
-        labels_id = self.tokenizer.encode(text=self.labels_map[label], add_special_tokens=False)
+        labels_id = self.tokenizer.encode(text=self.labels_map[label],
+                                          add_special_tokens=True)  # 在末尾添加 </s>
 
         return {
             "input_ids": text_encoded["input_ids"],
@@ -35,13 +35,13 @@ class MyDataset(Dataset):
 
     def collate_fn(self, batch):
         input_ids_list = [torch.tensor(instance['input_ids']) for instance in batch]
-        input_ids_pad = pad_sequence(input_ids_list, batch_first=True, padding_value=self.tokenizer.eos_token_id)
+        input_ids_pad = pad_sequence(input_ids_list, batch_first=True, padding_value=self.tokenizer.pad_token_id)
 
         attention_mask_list = [torch.tensor(instance['attention_mask']) for instance in batch]
         attention_mask_pad = pad_sequence(attention_mask_list, batch_first=True, padding_value=0)
 
         labels_list = [torch.tensor(instance['labels']) for instance in batch]
-        labels_pad = pad_sequence(labels_list, batch_first=True, padding_value=self.tokenizer.eos_token_id)
+        labels_pad = pad_sequence(labels_list, batch_first=True, padding_value=self.tokenizer.pad_token_id)
 
         return {
             "input_ids": torch.tensor(input_ids_pad),
